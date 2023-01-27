3eda4446d00017a9c75195c1359c52f5f5c8fe60
eavgrey <81044186+eavgrey@users.noreply.github.com>
2023-01-27 18:32:44 +0300

    feat: fix bugs

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ module.exports = function check(str, bracketsConfig) {
   let brackets = bracketsConfig.map((item) => {
     return `${item[0]}${item[1]}`;
   });
-  console.log(brackets);
+  let startLength = str.length;
 
   while (str.length) {
     brackets.forEach((item) => {
       str = str.replaceAll(item, "");
     });
+
+    if (str.length === startLength) {
+      return false;
+    }
+
+    startLength = str.length;
   }
-  if (str.length === 0) {
-    return true;
-  } else {
-    return false;
-  }
+
+  return true;
 };
