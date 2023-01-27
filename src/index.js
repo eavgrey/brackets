26274a8cb292c82e543b2acda3606ab575de9b9d
eavgrey <81044186+eavgrey@users.noreply.github.com>
2023-01-27 18:21:33 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let brackets = bracketsConfig.map((item) => {
+    return `${item[0]}${item[1]}`;
+  });
+  console.log(brackets);
+
+  while (str.length) {
+    brackets.forEach((item) => {
+      str = str.replaceAll(item, "");
+    });
+  }
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
